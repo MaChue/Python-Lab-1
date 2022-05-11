6062dc9fa0d13aeff879f9a265da7a95d05f05b5
Ma Chue <ma.chue.mc@gmail.com>
2022-05-11 17:03:06 +0000

    Implement add/delete/search algorithm

--- data.py
+++ data.py
@@ -8,6 +8,42 @@ class Person:
     self.address = address
     self.company = company
 
-  def get_fullname(self):
+  def get_fullname(self)-> str:
     return self.first_name + " " + self.last_name
 
+# Array that will maintain list of Person objects shown in Address Book
+persons = []
+
+# Function to add new person to Address Book
+def add_new_person(new_person: Person):
+  if len(persons) == 0:
+    persons.append(new_person)
+  else:
+    low = 0
+    high = len(persons)
+    while low < high:
+      middle = (low + high) // 2
+      if persons[middle].first_name.lower() < new_person.first_name.lower():
+        low = middle + 1
+      else:
+        high = middle
+    persons.insert(low, new_person)
+
+# Function to delete person from Address Book 
+def delete_person(index: int):
+  if index >= 0 and index < len(persons):
+    persons.pop(index)
+
+# Function to search person by comparing with full name and inputed keyword
+def search_person_by_keyword(keyword: str)->[Person]:
+  result = []
+  for person in persons:
+    full_name = person.get_fullname().lower()
+    try:
+      person.get_fullname().lower().index(keyword.lower())
+    except Exception as e:
+      continue
+    else :
+      result.append(person)
+  return result
+  
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import data
 
 p1 = data.Person("John", "Smith", 6591034343, "johnsmith@gmail.com", "London, UK", "Amazon") 
-print(p1.get_fullname()," Phone: ", p1.phone)
+p2 = data.Person("Susan", "Smith", 659603433, "susansmith@gmail.com", "London, UK", "Apple") 
+p3 = data.Person("Amy", "Jame", 33534532432, "amyjame@gmail.com", "London, UK", "Google") 
+
+data.add_new_person(p1)
+data.add_new_person(p2)
+data.add_new_person(p3)
+for person in data.persons:
+  print(person.get_fullname())
+print("........................")
+
+result = data.search_person_by_keyword("Smith")
+for r in result:
+  print(r.get_fullname())
+print("........................")
+
+data.delete_person(1)
+for person in data.persons:
+  print(person.get_fullname())
+print("........................")
